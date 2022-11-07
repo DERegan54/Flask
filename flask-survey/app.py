f9b8835f9aa7bbf3e49bd4858ba58a48e419e201
Danielle Regan <regandanielle54@gmail.com>
2022-11-07 14:31:34 -0700

    Add files via upload

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
+RESPONSES_KEY = "responses"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "jello"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 satisfaction_survey_instructions = "satisfaction_survey.instructions"
-
 
 @app.route("/")
 def display_start_page():
@@ -19,12 +19,36 @@ def display_start_page():
 
 @app.route("/start", methods=["POST"])
 def start_survey():
+    """Empty the session responses."""
+
+    session[RESPONSES_KEY] = []
+
     return redirect("/questions/0")
     
+
+@app.route("/answer", methods=["POST"])
+def handle_answer():
+    """Save answer and redirect to next question."""
+
+    #extract the answer choice
+    choice = request.form['answer']
+
+    #add answer to the responses list
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        #survey is complete, redirect to thank you page.
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
 
 @app.route("/questions/<int:qid>")
 def display_question(qid):
     """Display current question."""
+    responses = session.get(RESPONSES_KEY)
 
     if (responses is None):
         # Trying to answer questions before starting the survey
@@ -42,21 +66,6 @@ def display_question(qid):
         "question_form.html", question_num=qid, question=question)
     
 
-@app.route("/answer", methods=["POST"])
-def handle_answer():
-    """Save answer and redirect to next question."""
-
-    #extract the answer choice
-    choice = request.form['answer']
-
-    #add answer to the responses list
-    responses.append(choice)
-
-    if (len(responses) == len(survey.questions)):
-        #survey is complete, redirect to thank you page.
-        return redirect("/complete")
-    else:
-        return redirect (f"/questions/{len(responses)}")
 
 @app.route("/complete")
 def complete():
